7e903c361a0e8d662041ee0a8844f07c1116068e
ljt <liangjting@gmail.com>
2021-04-02 11:43:44 +0800

    曲线图line：增加标注最高值选项

--- src/components/drawLineChart.js
+++ src/components/drawLineChart.js
@@ -3,6 +3,57 @@ import { getRelativeRegion } from './utils'
 function coordFromPencentage([xp, yp], {left, bottom, width, height}) {
     return [xp * width + left, bottom - yp * height]
 }
+
+/**
+ * 
+ * @param {*} context 
+ * @param {{point: [number, number], label: string}} toptip 
+ * @param {*} region 
+ * @param {*} opts 
+ */
+function drawToptips(context, toptip, region, opts={}) {
+    let fontColor = 'white'
+    let fontSize = 14
+    let pointColor = opts.maxvalColor || '#ff5757'
+    let pointRadius = 3
+    let labelPadding = 2
+    let padding = 6
+    context.save()
+    context.fillStyle = pointColor
+    context.beginPath()
+    context.arc(...toptip.point, pointRadius, 0, 2 * Math.PI)
+    context.fill()
+
+    let [x, y] = toptip.point
+    let labelWidth = context.measureText(toptip.label).width + labelPadding * 2
+    let labelHeight = fontSize + labelPadding * 2
+
+    
+
+    if (y - labelHeight / 2 - padding > region.top) {
+        y -= labelHeight / 2 + padding
+    } else {
+        y += labelHeight / 2 + padding
+    }
+    if (x - labelWidth / 2 < region.left) {
+        x = region.left + labelWidth / 2
+    } else if (x + labelWidth / 2 > region.right) {
+        x = region.right - labelWidth / 2
+    }
+
+
+    let leftTop = [x - labelWidth / 2, y - labelHeight / 2]
+
+    console.log('lefttop', leftTop, labelWidth, labelHeight)
+    context.fillRect(...leftTop, labelWidth, labelHeight)
+
+    context.fillStyle = fontColor
+    context.font = `${fontSize}px sans-serif`
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.fillText(toptip.label, x, y)
+    context.restore()
+}
 /**
  * 
  * @param {*} context 
@@ -17,6 +68,7 @@ export default function drawLineChart(context, series, opts, config) {
     let axis = calcAxisSeries(series, opts, config)
     let result = drawAxis(context, axis, region, opts, config)
     let cRegion = result.region
+    let showMaxval = opts.showMaxval || false
     // console.log(axis, result)
     // context.fillStyle = 'red'
     // context.fillRect(cRegion.left, cRegion.top, cRegion.width, cRegion.height)
@@ -24,7 +76,18 @@ export default function drawLineChart(context, series, opts, config) {
     let xRange = axis.xRange || 10
     let yMax = axis.yMax || 10
     let xOrigin = result.xOrigin || 0
-    let points = series.map(([x, y]) => [(x - xOrigin) / xRange, y/ yMax])
+    let maxVal, maxValPoint
+    let points = series.map(([x, y]) => {
+        let p = [(x - xOrigin) / xRange, y/ yMax]
+        if (maxVal === undefined) {
+            maxVal = y
+            maxValPoint = p
+        } else if (y > maxVal) {
+            maxVal = y
+            maxValPoint = p
+        }
+        return p
+    })
     // console.log(points)
     if (points.length > 0) {
         context.beginPath()
@@ -33,6 +96,11 @@ export default function drawLineChart(context, series, opts, config) {
             context.lineTo(...coordFromPencentage(p, cRegion))
         }
         context.stroke()
+    }
+    if (showMaxval && maxValPoint) {
+        console.log(maxValPoint, maxVal)
+        maxValPoint = [...coordFromPencentage(maxValPoint, cRegion)]
+        drawToptips(context, {point: maxValPoint, label: maxVal}, cRegion, opts)
     }
     
 }
